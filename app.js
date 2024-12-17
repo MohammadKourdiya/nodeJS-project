daa9ca12fbe6e86be9e6f50ba726348533411eac
Mohammad Kourdiya <mkourdiya@gmail.com>
2024-12-17 16:29:44 +0300

    show errors in frontend

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 const mongoose = require("mongoose");
 const allRoutes = require("./routes/allRoutes");
